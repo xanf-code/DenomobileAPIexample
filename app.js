56dabe67c2e3c3d2ef10e89c6c47bfa128de8c23
darshanaswath <39420536+darshanaswath@users.noreply.github.com>
2020-07-24 12:08:11 +0530

    changes

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
 
 const env = Deno.env.toObject();
+const PORT = env.PORT || 4000;
+const HOST = env.HOST || 'localhost';
+
+const router = new Router();
